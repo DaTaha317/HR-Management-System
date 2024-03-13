9d5e1b237bc943d720bb5855fdb25f143d886924
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-03-14 00:10:48 +0200

    Migration v3
    renamed OfficalDaysOff model into DaysOff

--- WebAPI/WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using WebAPI.DTOs;
+using WebAPI.DTOs.Department;
 using WebAPI.Interfaces;
 using WebAPI.Models;
 
--- WebAPI/WebAPI/DTOs/Department/DepartmentDTO.cs
+++ WebAPI/WebAPI/DTOs/Department/DepartmentDTO.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.DTOs
+namespace WebAPI.DTOs.Department
 {
     public class DepartmentDTO
     {
