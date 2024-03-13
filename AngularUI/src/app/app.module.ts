e740397c01f7e48cc2c531b403f0da1af516dad7
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-03-13 12:15:03 +0200

    Setting up AngularUI with Bootstrap and some necessary Modules
    - HttpClientModule
    - FormsModule

--- AngularUI/src/app/app.module.ts
+++ AngularUI/src/app/app.module.ts
@@ -1,18 +1,17 @@
+// Importing Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// Importing Components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
