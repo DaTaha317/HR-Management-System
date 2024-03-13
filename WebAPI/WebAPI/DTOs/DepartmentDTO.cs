ba462c54b8e579caa9ab88e93f8f84052fabb0e2
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-03-13 20:51:44 +0200

    Implemented DepartmentRepo and controller

---
+++ WebAPI/WebAPI/DTOs/DepartmentDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.DTOs
+{
+    public class DepartmentDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Repositories/DepartmentRepo.cs
@@ -0,0 +1,19 @@
+﻿using WebAPI.Interfaces;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+    public class DepartmentRepo : IDepartmentRepo
+    {
+        private HRDBContext dBContext;
+
+        public DepartmentRepo(HRDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public List<Department> GetAll()
+        {
+            return dBContext.Departments.ToList();
+        }
+    }
+}
