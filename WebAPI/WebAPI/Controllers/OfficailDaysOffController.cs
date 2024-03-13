5caa6fe7642d70c1e0b2adb68b6c52c43e12bc77
Mostafa-Elzareef79 <mostafaelzareef79@gmail.com>
2024-03-13 19:10:03 +0200

    create OfficalDaysOffRepo and impmlement this interface

---
+++ WebAPI/WebAPI/Controllers/OfficailDaysOffController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class OfficailDaysOffController : ControllerBase
+	{
+        public OfficailDaysOffController()
+        {
+            
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Repositories/OfficalDaysOffRepo.cs
@@ -0,0 +1,51 @@
+﻿using WebAPI.Interfaces;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+	public class OfficalDaysOffRepo : IOfficialDaysOff
+	{
+		private HRDBContext OfficalDaysOffContext;
+
+		public OfficalDaysOffRepo(HRDBContext OfficalDaysOffContext)
+        {
+            this.OfficalDaysOffContext = OfficalDaysOffContext;
+        }
+        public void Add(OfficialDaysOff dayOff)
+		{
+			OfficalDaysOffContext.OfficialDaysOffs.Add(dayOff);
+			
+
+
+		}
+
+		public void Delete(DateOnly day)
+		{
+			OfficalDaysOffContext.Remove(GetByDay(day));
+		}
+
+		public List<OfficialDaysOff> GetAll()
+		{
+		return OfficalDaysOffContext.OfficialDaysOffs.ToList();
+		}
+
+		public OfficialDaysOff GetByDay(DateOnly day)
+		{
+			return OfficalDaysOffContext.OfficialDaysOffs.SingleOrDefault(i => i.Date == day);
+		}
+
+		public void Save()
+		{
+			OfficalDaysOffContext.SaveChanges();
+		}
+
+		public void Update(DateOnly day, OfficialDaysOff dayOff)
+		{
+			if(GetByDay(day) != null)
+			{
+              OfficalDaysOffContext.OfficialDaysOffs.Update(dayOff);
+			}
+			
+		}
+	}
+}
