12e39d4d4b49abdb7ba50fb82b97f27b53993312
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-03-15 19:17:53 +0200

    + Adding Organization model, Commission and Deduction = Then implementing OrganizationController to update commission and deduction settings

---
+++ WebAPI/WebAPI/Interfaces/IDeduction.cs
@@ -0,0 +1,14 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Interfaces
+{
+    public interface IDeduction
+    {
+        public void Add(DeductionSettings deduction);
+
+        public void Update(DeductionSettings deduction);
+
+        public DeductionSettings? Get();
+        public void Save();
+    }
+}
