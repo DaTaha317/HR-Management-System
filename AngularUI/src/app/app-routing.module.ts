caae6ecc1a4ed7032a541e2e699f8976cf9c015b
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-03-17 00:26:35 +0200

    adding router link

--- AngularUI/src/app/app-routing.module.ts
+++ AngularUI/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { OrganizationSettingsComponent } from './components/organization-settings/organization-settings.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'login', component: SignInComponent }
+  { path: 'login', component: SignInComponent },
+  {path: 'settings', component: OrganizationSettingsComponent}
 ];
 
 @NgModule({
